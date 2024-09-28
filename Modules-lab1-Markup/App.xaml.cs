e3b141c08814158552d9ce8c899c0e6cd235f32d
BodinAE <BodinAE@319D-3>
2024-09-28 11:02:40 +0700

    Добавьте файлы проекта.

---
+++ Modules-lab1-Markup/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Modules_lab1_Markup
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
